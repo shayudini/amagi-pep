60d84513c1e0ad92f28785084648a0f169c4f111
Sharif Hayudini <shayudini@gmail.com>
2022-06-24 10:38:39 +0300

    update layout

--- src/base/Layouts/AboutLayout.js
+++ src/base/Layouts/AboutLayout.js
@@ -3,7 +3,7 @@ import { Navigation } from 'components/index';
 
 const AboutLayout = ({ children }) => {
   return (
-    <main className="flex h-full w-full flex-col lg:flex-row">
+    <main className="flex h-full w-full flex-col sm:flex-row md:flex-row lg:flex-row">
       <Navigation />
       <section className="relative h-full w-full overflow-hidden">
         {children}
